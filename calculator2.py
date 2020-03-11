3b67ee537e8e32f8dec14e245f37f4c94511e443
Fzty <51758889+fsty@users.noreply.github.com>
2020-03-11 21:59:48 +0100

    Update calculator2.py

--- calculator2.py
+++ calculator2.py
@@ -8,12 +8,6 @@ def main():
     num2 = float(input("Enter another number: "))
 
 
-
-    ############################################
-### Ignore the comments below ###### 
-    #simple algebra, this is not CAS and will not accept () ^x and other algebra symbols 
-    # however, it will give the variable a value, essentially 1, so say that X = 1, you can add X into the equation doing simple algebra using charachters in math
-
     if operator == "+":
         print(num1 + num2)
     elif operator == "-":
@@ -37,5 +31,3 @@ def main():
 
 
 
-
-#################################
